e87698c262e739bc5887ab5d1656ecaf0ee459cf
AVS-2307 <andrey230781.an@gmail.com>
2023-03-16 11:54:38 +0300

    code made with cookie (remove localStorage)

--- popup/task.js
+++ popup/task.js
@@ -1,16 +1,30 @@
 const modalopen = document.querySelector('.modal');
 const closeModal = document.querySelector('.modal__close');
 
+function setCookie(name, value) {
+    document.cookie = name + '=' + encodeURIComponent(value)
+    console.log(document.cookie);
+};
+
+function getCookie(name) {
+    const pairs = document.cookie.split('; ');
+    const cookie = pairs.find(p => p.startsWith(name + '=')) //находим имя cookie
+    console.log(cookie.substr(name.length + 1)) 
+    return cookie.substr(name.length + 1) //оставляем только значение cookie
+};
+
+
 function handleModalOpen() {
     modalopen.classList.add('modal_active');
 };
 
 function handleModalClose() {
-    modalopen.classList.remove('modal_active');
-    localStorage.setItem('modal_closed', 'true');
+    modalopen.classList.remove('modal_active');    
+    setCookie('modal_closed', true);
+
 }; 
 
-if (localStorage.getItem('modal_closed') !== 'true') {
+if (getCookie('modal_closed') !== true) {
     handleModalOpen();
 };
 
